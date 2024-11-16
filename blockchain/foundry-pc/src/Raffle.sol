c2380ae7a9b34903bf2b151fc02181d338b5933b
Lakshmi.Sanikommu <59156926+18121A05L2@users.noreply.github.com>
2024-11-16 22:58:18 +0530

    init

--- blockchain/foundry-pc/src/Raffle.sol
+++ blockchain/foundry-pc/src/Raffle.sol
@@ -25,11 +25,11 @@ pragma solidity 0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
-import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 
 /**
  * @title A sample Raffle Contract
- * @author Patrick Collins
+ * @author Lakshmi Sanikommu
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
